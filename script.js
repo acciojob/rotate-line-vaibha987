ae9ee019419f160186fad225812c1d55f139e3ed
vaibha987 <v2goyal95@gmail.com>
2024-09-02 22:27:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+
+let line = document.getElementById("line");
+let angle = 0;
+function rotate() {
+    angle += 2;
+    line.style.transform = `rotate(${angle}deg)`;
+}
+setInterval(rotate, 20);
